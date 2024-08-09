77c856e4e28aa5d69ced4ffb4c25117722ad3d91
paulleadbeater1987 <paul_leadbeater@live.co.uk>
2024-08-09 12:25:01 +0700

    deploy.bat log updates

--- deploy.bat
+++ deploy.bat
@@ -5,11 +5,25 @@ set LOCAL_DIR=C:\xampp\htdocs\paulleadbeater
 set REMOTE_DIR=/
 set LOG_FILE=C:\xampp\htdocs\paulleadbeater\log\file.log
 
+:: Prompt for FTP password
 set /p PASS="FTP password: "
 
-"C:\Program Files (x86)\WinSCP\WinSCP.com" /log=%LOG_FILE% /command ^
-  "open ftp://%USER%:%PASS%@%HOST%" ^
-  "lcd %LOCAL_DIR%" ^
-  "cd %REMOTE_DIR%" ^
-  "put -neweronly * -filemask=""|.git/""" ^
-  "exit"
+:: Run WinSCP command and log output
+(
+    echo %date% %time% Starting deployment...
+
+    "C:\Program Files (x86)\WinSCP\WinSCP.com" /log=%LOG_FILE% /command ^
+      "open ftp://%USER%:%PASS%@%HOST%" ^
+      "lcd %LOCAL_DIR%" ^
+      "cd %REMOTE_DIR%" ^
+      "put -neweronly * -filemask=""|.git/""" ^
+      "exit"
+
+    :: Check if WinSCP executed successfully
+    if %ERRORLEVEL% neq 0 (
+        echo %date% %time% WinSCP deployment failed with error code %ERRORLEVEL% >> %LOG_FILE%
+        exit /b %ERRORLEVEL%
+    ) else (
+        echo %date% %time% WinSCP deployment completed successfully. >> %LOG_FILE%
+    )
+) >> %LOG_FILE% 2>&1
